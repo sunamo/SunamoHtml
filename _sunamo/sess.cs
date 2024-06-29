62d28d5b8f1d16e3b3ee0824aff3a6a3de834050
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-29 07:56:37 +0200

    moved all in _sunamo to right subfolders by original project

--- _sunamo/sess.cs
+++
@@ -1,8 +0,0 @@
-namespace SunamoHtml;
-internal class sess
-{
-    internal static string i18n(string metodaHtmlGenerator2TopListWithImagesOdkazyPhoto)
-    {
-        return metodaHtmlGenerator2TopListWithImagesOdkazyPhoto;
-    }
-}
