c705a1f2c8efca3f94e5b13458316284e296f874
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-29 11:37:48 +0200

    Make some classes public when it is needed

---
+++ _sunamo/SunamoArgs/ChangeContentArgsHtml.cs
@@ -0,0 +1,9 @@
+namespace SunamoHtml;
+
+
+internal class ChangeContentArgsHtml
+{
+    internal bool removeNull = false;
+    internal bool removeEmpty = false;
+    internal bool switchFirstAndSecondArg = false;
+}
