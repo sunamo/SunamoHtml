4ddfc8fb7c9583066bf1509d24457e7234a98dfb
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-30 15:08:45 +0200

    .

--- Html/HtmlDocumentS.cs
+++ Html/HtmlDocumentS.cs
@@ -3,6 +3,17 @@ namespace SunamoHtml;
 
 public static partial class HtmlDocumentS
 {
+    private static string s_html2 = null;
+    public static HtmlNode LoadHtml(string html)
+    {
+        HtmlDocument hd = HtmlAgilityHelper.CreateHtmlDocument();
+
+        html = WebUtility.HtmlDecode(html);
+        s_html2 = html;
+        //HtmlHelper.ToXml(html)
+        hd.LoadHtml(html);
+        return hd.DocumentNode;
+    }
 
     public static
 #if ASYNC
