08a74e65a1d71ee6ce98e3e97447c71ce502fd0a
sunamo.cz <radek.jancik@sunamo.cz>
2025-02-01 08:45:07 +0100

    Added a better self-descriptive message

--- _sunamo/SunamoExceptions/Exceptions.cs
+++ _sunamo/SunamoExceptions/Exceptions.cs
@@ -94,7 +94,7 @@ bool fillAlsoFirstTwo = true)
                 notContained.Add(item);
         return notContained.Count == 0
         ? null
-        : CheckBefore(before) + originalText + " dont contains: " + string.Join(",", notContained);
+        : CheckBefore(before) + "Original text dont contains: " + string.Join(",", notContained) + ". Original text: " + originalText;
     }
     internal static string? IsEmpty(string before, IEnumerable folders, string colName,
     string additionalMessage = "")
